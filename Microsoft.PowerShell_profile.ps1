3b62b730ed69940062df993c64295574f2995e09
Diego Barrio Hortigüela <diegobarrioh@gmail.com>
2013-06-06 10:00:27 +0200

    ls & dir colored Aliases, with script New-CommandWrapper

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,5 @@
-Set-Alias rc Edit-PowershellProfile
 Set-Alias subl 'C:\Program Files\Sublime Text 2\sublime_text.exe'
+Set-Alias rc Edit-PowershellProfile
 
 $a = (Get-Host).UI.RawUI
 $a.BackgroundColor = "black"
@@ -8,6 +8,8 @@ $a.WindowTitle = "PowerShell Windows 8"
 
 $strUser = $env:username
 $strHost = $env:computername
+
+$env:Path += ";$(Split-Path $profile)\Scripts"
 
 function Prompt
 {
@@ -26,5 +28,100 @@ function Prompt
 
 function Edit-PowershellProfile
 {
-    notepad $Profile
+    subl $Profile
 }
+
+New-CommandWrapper Out-Default -Process {
+  $regex_opts = ([System.Text.RegularExpressions.RegexOptions]::IgnoreCase)
+  $compressed = New-Object System.Text.RegularExpressions.Regex(
+    '\.(zip|tar|gz|rar|jar|war)$', $regex_opts)
+  $executable = New-Object System.Text.RegularExpressions.Regex(
+    '\.(exe|bat|cmd|msi|ps1|psm1|vbs|reg)$', $regex_opts)
+
+  if(($_ -is [System.IO.DirectoryInfo]) -or ($_ -is [System.IO.FileInfo]))
+  {
+    if(-not ($notfirst))
+    {
+      Write-Host "`n    Directory: " -noNewLine
+      Write-Host "$(pwd)`n" -foregroundcolor "Blue"
+      Write-Host "Mode        Last Write Time       Length   Name"
+      Write-Host "----        ---------------       ------   ----"
+      $notfirst=$true
+    }
+
+    if ($_ -is [System.IO.DirectoryInfo])
+    {
+      Write-Host ("{0}   {1}                {2}" -f $_.mode, ([String]::Format("{0,10} {1,8}", $_.LastWriteTime.ToString("d"), $_.LastWriteTime.ToString("t"))), $_.name) -ForegroundColor "Blue"
+    }
+    else
+    {
+      if ($compressed.IsMatch($_.Name))
+      {
+        $color = "Red"
+      }
+      elseif ($executable.IsMatch($_.Name))
+      {
+        $color =  "Green"
+      }
+      else
+      {
+        $color = "White"
+      }
+      Write-Host ("{0}   {1}   {2,10}   {3}" -f $_.mode, ([String]::Format("{0,10} {1,8}", $_.LastWriteTime.ToString("d"), $_.LastWriteTime.ToString("t"))), $_.length, $_.name) -ForegroundColor $color
+    }
+
+    $_ = $null
+  }
+} -end {
+  Write-Host
+}
+
+function Get-DirSize
+{
+  param ($dir)
+  $bytes = 0
+  $count = 0
+
+  Get-Childitem $dir | Foreach-Object {
+    if ($_ -is [System.IO.FileInfo])
+    {
+      $bytes += $_.Length
+      $count++
+    }
+  }
+
+  Write-Host "`n    " -NoNewline
+
+  if ($bytes -ge 1KB -and $bytes -lt 1MB)
+  {
+    Write-Host ("" + [Math]::Round(($bytes / 1KB), 2) + " KB") -ForegroundColor "White" -NoNewLine
+  }
+  elseif ($bytes -ge 1MB -and $bytes -lt 1GB)
+  {
+    Write-Host ("" + [Math]::Round(($bytes / 1MB), 2) + " MB") -ForegroundColor "White" -NoNewLine
+  }
+  elseif ($bytes -ge 1GB)
+  {
+    Write-Host ("" + [Math]::Round(($bytes / 1GB), 2) + " GB") -ForegroundColor "White" -NoNewLine
+  }
+  else
+  {
+    Write-Host ("" + $bytes + " bytes") -ForegroundColor "White" -NoNewLine
+  }
+  Write-Host " in " -NoNewline
+  Write-Host $count -ForegroundColor "White" -NoNewline
+  Write-Host " files"
+
+}
+
+function Get-DirWithSize
+{
+  param ($dir)
+  Get-Childitem $dir
+  Get-DirSize $dir
+}
+
+Remove-Item alias:dir
+Remove-Item alias:ls
+Set-Alias dir Get-DirWithSize
+Set-Alias ls Get-DirWithSize
